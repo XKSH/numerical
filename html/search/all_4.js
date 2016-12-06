55ba1dc3f8eb4449fabbb15e2fccb3818cbc13b6
antpos <anttiposo@gmail.com>
2016-12-06 22:13:08 +0200

    fixed link in README

--- html/search/all_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['print',['print',['../doxygenTest_8cpp.html#a853ca2097c5c2da1a8eac6b1148e7ff6',1,'doxygenTest.cpp']]]
-];
--- html/search/files_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['doxygentest_2ecpp',['doxygenTest.cpp',['../doxygenTest_8cpp.html',1,'']]]
-];
