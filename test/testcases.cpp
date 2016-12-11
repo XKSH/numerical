b3f357387033bd5cb8dc1f984d6255b8d8b4b5cf
antpos <anttiposo@gmail.com>
2016-12-11 21:24:45 +0200

    Minor fixes

--- test/testcases.cpp
+++ test/testcases.cpp
@@ -1,5 +1,6 @@
 #define BOOST_TEST_MODULE roots test
 #include <boost/test/included/unit_test.hpp>
+#include <iostream>
 #include "../src/roots.hpp"
 
 double f1(double x) { return x; } 		// Line with positive slope
@@ -10,10 +11,10 @@ double f4(double x) { return x*x-2; }
 double eps = 1e-6;
 
 BOOST_AUTO_TEST_CASE(roots){
-	BOOST_CHECK(abs(bisection(f1, -1, 1, eps, 1000)) < eps);
-	BOOST_CHECK(abs(bisection(f2, -1, 1, eps, 1000)) < eps);
-	BOOST_CHECK(!(abs(bisection(f3, 0, 1, eps, 1000)) < eps));
-	BOOST_CHECK(abs(bisection(f4, 0, 3, eps, 1000)) <= eps);
-	BOOST_CHECK(abs(bisection(f4, -3, 0, eps, 1000)) <= eps);
+	BOOST_CHECK(abs(bisection(f1, -1, 1, eps, 1000)) <= 0 + eps);
+	BOOST_CHECK(abs(bisection(f2, -1, 1, eps, 1000)) <= 0 + eps);
+	BOOST_CHECK(abs(bisection(f3, 0, 1, eps, 1000)) <= 0 + eps);
+	BOOST_CHECK(abs(bisection(f4, 0, 3, eps, 1000)) <= 1.1412 + eps);
+	BOOST_CHECK(abs(bisection(f4, -3, 0, eps, 1000)) <= 1.1412 +  eps);
 
 }
